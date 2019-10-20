b4772bd96f815816c9ed166ee743cb55f4cd7fed
Tomasz Niedziela-Brach <niedziela.brach@gmail.com>
2019-10-20 23:08:58 +0200

    Cleanup of ingored files

--- app/reserved/rooms/models.py
+++ app/reserved/rooms/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+
 class City(models.Model):
+    class Meta:
+        verbose_name_plural = 'cities'
+
     id = models.CharField(max_length=2, primary_key=True)
     name = models.CharField(max_length=255)
 
