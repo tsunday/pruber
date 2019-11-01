5c7779e7fecaac1e016742119146d71fa4ece408
Tomasz Niedziela-Brach <niedziela.brach@gmail.com>
2019-11-02 00:28:18 +0100

    Mypy setip

--- app/reserved/rooms/repository.py
+++ app/reserved/rooms/repository.py
@@ -2,5 +2,5 @@ from django.db.models import QuerySet
 
 
 class RoomQuerySet(QuerySet):
-    def available(self):
+    def available(self) -> QuerySet:
         return self.filter(reservations__isnull=True)
