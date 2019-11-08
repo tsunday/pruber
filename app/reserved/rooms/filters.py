fb1818f5df4787deb94c5bfbeaf50238e582267d
Tomasz Niedziela-Brach <niedziela.brach@gmail.com>
2019-11-08 23:08:21 +0100

    Date filtering

--- app/reserved/rooms/filters.py
+++ app/reserved/rooms/filters.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+
 from django.db.models import QuerySet
+from rest_framework.exceptions import ValidationError
 from rest_framework.filters import BaseFilterBackend
 from rest_framework.request import Request
 
 
 class AvailabilityFilterBackend(BaseFilterBackend):
     def filter_queryset(self, request: Request, queryset: QuerySet, view) -> QuerySet:
-        return queryset.available(
-            dt_from=request.query_params.get('from', None), dt_to=request.query_params.get('to', None)
-        )
+        try:
+            dt_from = datetime.fromisoformat(request.query_params.get('from', ''))
+            dt_to = datetime.fromisoformat(request.query_params.get('to', ''))
+        except ValueError as e:
+            raise ValidationError(str(e))
+        return queryset.available(dt_from=dt_from, dt_to=dt_to)
