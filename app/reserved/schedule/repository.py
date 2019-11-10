936b30084480baf910e2a81db4f6001c81eb60c3
Tomasz Niedziela-Brach <niedziela.brach@gmail.com>
2019-11-10 23:00:13 +0100

    Reserved rooms logic works

--- app/reserved/schedule/repository.py
+++ app/reserved/schedule/repository.py
@@ -6,5 +6,6 @@ from django.db.models import QuerySet, Q
 class ScheduleQuerySet(QuerySet):
     def reserved_rooms(self, dt_from: datetime, dt_to: datetime):
         return self.filter(
-            Q(datetime_from__lt=dt_to, datetime_to__gte=dt_to)
-        ).values_list('room').distinct()
+            Q(datetime_from__lte=dt_from, datetime_to__lte=dt_to, datetime_to__gt=dt_from) |
+            Q(datetime_from__gte=dt_from, datetime_from__lt=dt_to)
+        ).distinct().values_list('room', flat=True)
